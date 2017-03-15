a468e219f2d72740cedca30136d8f5674c6b7d0d
xiowang <henrywangx@gmail.com>
2017-03-15 21:35:26 +0800

    change README.md

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # crawler
 This object of this crawler is to get the information in special field from muchong.com.
+## Run
+
+```shell
+npm install
+node index.js
+```
+## Visit
+[localhost:8000](localhost:8000)
 
--- src/getList.js
+++ src/getList.js
@@ -1,7 +1,7 @@
 var request = require('request');
 var iconv = require('iconv-lite');
 
-//
+// The start url(小木虫上，计算机应用技术相关的期刊)
 const urlStart = "http://muchong.com/bbs/journal_cn.php?tag=%BC%C6%CB%E3%BB%FA%D3%A6%D3%C3%BC%BC%CA%F5"
 const regExp = /<th\s+>\s+<a\s+href="(.{30,40})">(.{4,20})<\/a>/ig;
 
